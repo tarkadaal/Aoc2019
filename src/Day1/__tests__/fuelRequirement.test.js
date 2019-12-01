c1840f9a6864241da75f792710767bec9f493681
Dan Jackson <danjacksonis@googlemail.com>
2019-12-01 22:46:34 +0100

    Part 2 of day 1

--- src/Day1/__tests__/fuelRequirement.test.js
+++ src/Day1/__tests__/fuelRequirement.test.js
@@ -1,10 +1,10 @@
 /* eslint-env jest */
-import { calculateFuelRequirement } from '../fuelCalculator'
+import { calculateFuel, calculateFuelEx } from '../fuelCalculator'
 
 describe('Fuel Requirement Calculator', () => {
   test('exists', () => {
     expect(() => {
-      calculateFuelRequirement(10)
+      calculateFuel(10)
     }).not.toThrow()
   })
   test.each([
@@ -15,7 +15,19 @@ describe('Fuel Requirement Calculator', () => {
   ])(
     'Testing with mass: %p',
     ({ mass, expected }) => {
-      const actual = calculateFuelRequirement(mass)
+      const actual = calculateFuel(mass)
+      expect(actual).toEqual(expected)
+    })
+
+  test.each([
+    [{ mass: 12, expected: 2 }],
+    [{ mass: 14, expected: 2 }],
+    [{ mass: 1969, expected: 966 }],
+    [{ mass: 100756, expected: 50346 }]
+  ])(
+    'Testing with mass: %p',
+    ({ mass, expected }) => {
+      const actual = calculateFuelEx(mass)
       expect(actual).toEqual(expected)
     })
 })
--- src/Day1/fuelCalculator.js
+++ src/Day1/fuelCalculator.js
@@ -1,7 +1,13 @@
-const calculateFuelRequirement = (mass) => {
+const calculateFuel = (mass) => {
   return Math.floor(mass / 3) - 2
 }
 
+const calculateFuelEx = (mass) => {
+  const fuel = calculateFuel(mass)
+  return fuel <= 0 ? 0 : fuel + calculateFuelEx(fuel)
+}
+
 export {
-  calculateFuelRequirement
+  calculateFuel,
+  calculateFuelEx
 }
